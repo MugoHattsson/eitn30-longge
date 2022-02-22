5bc344a4623808dcd0d72e468ba7b8c02ec2b6f4
InternetInutiPi <notmy@email.com>
2022-02-22 13:05:30 +0000

    Changed incorrect masquerading parameter

--- forward.sh
+++ forward.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 iptables -A FORWARD -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
